f7bcacbe90b6296ac1bd2eab9860e469a3e91015
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2025-01-25 16:43:04 +0200

    slots table migration

---
+++ database/migrations/2025_01_24_184353_create_slots_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->dateTime('time');
+            $table->uuid('doctor_id');
+            $table->string('doctor_name');
+            $table->boolean('is_reserved')->default(false);
+            $table->decimal('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('slots');
+    }
+};
